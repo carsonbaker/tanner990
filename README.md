1a26e8c3c7db16a57a6bc6749f825ac1cf35132b
Carson Baker <carson@carsonbaker.org>
2018-08-09 15:20:31 -0700

    Extracting.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 References:
 
-https://docs.opendata.aws/irs-990/readme.html
-https://registry.opendata.aws/irs990/
-https://github.com/CharityNavigator/990_long
-https://medium.com/@open990/the-irs-990-e-file-dataset-getting-to-the-chocolatey-center-of-data-deliciousness-90f66097a600
+* https://docs.opendata.aws/irs-990/readme.html
+* https://registry.opendata.aws/irs990/
+* https://github.com/CharityNavigator/990_long
+* https://medium.com/@open990/the-irs-990-e-file-dataset-getting-to-the-chocolatey-center-of-data-deliciousness-90f66097a600
 
 Given:
 
@@ -13,4 +13,6 @@ Given:
 Prettify index_2017.json:
 `jq index_2017.json > index_2017-pretty.json`
 
+Master concordance file:
 
+* https://raw.githubusercontent.com/Nonprofit-Open-Data-Collective/irs-efile-master-concordance-file/master/efiler_master_concordance.csv
---
+++ extract.rb
@@ -0,0 +1,44 @@
+#!/bin/env ruby
+
+require 'csv'
+require 'oga'
+
+selectors = %w(
+  /Return/ReturnHeader/Filer/EIN
+  /Return/ReturnHeader/Filer/BusinessName/BusinessNameLine1Txt
+  /Return/ReturnData/IRS990/AccountsReceivableGrp/BOYAmt
+  /Return/ReturnData/IRS990/AccountsReceivableGrp/EOYAmt
+)
+
+# /Return/ReturnData/IRS990/ProgramServiceRevenueGrp/TotalRevenueColumnAmt
+# /Return/ReturnData/IRS990/ProgramServiceRevenueGrp/RelatedOrExemptFuncIncomeAmt
+
+csv_string = CSV.generate do |csv|
+
+  # output column names
+  csv << selectors.map { |s| s.split(/\//).last(2).join('-') }
+
+  ARGV.each do |a|
+
+    puts "Processing #{a}"
+
+    handle = File.open(a)
+    document = Oga.parse_xml(handle)
+
+    cols = []
+
+    selectors.each do |s|
+      el = document.at_xpath(s)
+      if el
+        cols << el.text
+      else
+        cols << nil
+      end
+    end
+
+    csv << cols
+  end
+
+end
+
+puts csv_string
